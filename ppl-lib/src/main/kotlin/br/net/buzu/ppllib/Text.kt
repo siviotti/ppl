e7a64192055b30aaa608ca1086ce6ab9f5e153d1
07311631769 <07311631769@serpro-1537186>
2018-10-31 21:31:43 -0200

    Text Lib

--- ppl-lib/src/main/kotlin/br/net/buzu/ppllib/Text.kt
+++ ppl-lib/src/main/kotlin/br/net/buzu/ppllib/Text.kt
@@ -19,198 +19,192 @@ package br.net.buzu.ppllib
 import br.net.buzu.pplspec.model.Align
 
 /**
- * Classe com métodos utilitários.
+ * Informa se a String é nula, vazia ou toda formada por espaços.
  *
- * @author Douglas Siviotti
- * @since 1.0
+ * <pre>
+ * return text == null || text.trim().length() == 0;
+</pre> *
+ *
+ * @param text
+ * O texto a ser avaliado.
+ * @return `true` se é vazia (Blank) ou `false` se não
+ * for.
  */
-class Text {
+fun isBlank(text: String?): Boolean {
+	return text == null || text.trim { it <= ' ' }.length == 0
+}
 
-    /**
-     * Informa se a String é nula, vazia ou toda formada por espaços.
-     *
-     * <pre>
-     * return text == null || text.trim().length() == 0;
-    </pre> *
-     *
-     * @param text
-     * O texto a ser avaliado.
-     * @return `true` se é vazia (Blank) ou `false` se não
-     * for.
-     */
-    fun isBlank(text: String?): Boolean {
-        return text == null || text.trim { it <= ' ' }.length == 0
-    }
+/**
+ * Completa ou corta uma String de forma que ela fique com um tamanho fixo
+ * definido como parâmetro.
+ *
+ * @param align
+ * O tipo de alinhamento (direita ou esquerda).
+ *
+ * @param original
+ * A Srtring original.
+ * @param finalSize
+ * O tamanho final que a String deve ter.
+ * @param c
+ * O caractere que preenche possíveis buracos.
+ * @return A String no tamanho certo, preenchida ou cortada de acordo com o
+ * tamanho.
+ */
+fun fit(align: Align, original: String, finalSize: Int, c: Char): String {
+	if (original.length == finalSize) {
+		return original
+	}
+	return if (original.length < finalSize) fill(align, original, finalSize, c) else cut(
+		align,
+		original,
+		finalSize
+	)
+}
 
-    /**
-     * Completa ou corta uma String de forma que ela fique com um tamanho fixo
-     * definido como parâmetro.
-     *
-     * @param align
-     * O tipo de alinhamento (direita ou esquerda).
-     *
-     * @param original
-     * A Srtring original.
-     * @param finalSize
-     * O tamanho final que a String deve ter.
-     * @param c
-     * O caractere que preenche possíveis buracos.
-     * @return A String no tamanho certo, preenchida ou cortada de acordo com o
-     * tamanho.
-     */
-    fun fit(align: Align, original: String, finalSize: Int, c: Char): String {
-        if (original.length == finalSize) {
-            return original
-        }
-        return if (original.length < finalSize) fill(align, original, finalSize, c) else cut(align, original, finalSize)
-    }
+/**
+ * Completa ou corta uma String de forma que ela fique com um tamanho fixo
+ * definido como parâmetro com alinhamento á DIREITA.
+ *
+ *
+ * @param original
+ * A Srtring original.
+ * @param finalSize
+ * O tamanho final que a String deve ter.
+ * @param c
+ * O caractere que preenche possíveis buracos.
+ * @return A String no tamanho certo, preenchida ou cortada de acordo com o
+ * tamanho.
+ */
+fun rightFit(original: String, finalSize: Int, c: Char): String {
+	if (original.length == finalSize) {
+		return original
+	}
+	return if (original.length < finalSize) rightFill(original, finalSize, c) else rightCut(original, finalSize)
+}
 
-    /**
-     * Completa ou corta uma String de forma que ela fique com um tamanho fixo
-     * definido como parâmetro com alinhamento á DIREITA.
-     *
-     *
-     * @param original
-     * A Srtring original.
-     * @param finalSize
-     * O tamanho final que a String deve ter.
-     * @param c
-     * O caractere que preenche possíveis buracos.
-     * @return A String no tamanho certo, preenchida ou cortada de acordo com o
-     * tamanho.
-     */
-    fun rightFit(original: String, finalSize: Int, c: Char): String {
-        if (original.length == finalSize) {
-            return original
-        }
-        return if (original.length < finalSize) rightFill(original, finalSize, c) else rightCut(original, finalSize)
-    }
+/**
+ * Completa ou corta uma String de forma que ela fique com um tamanho fixo
+ * definido como parâmetro com alinhamento á ESQUERDA.
+ *
+ *
+ * @param original
+ * A Srtring original.
+ * @param finalSize
+ * O tamanho final que a String deve ter.
+ * @param c
+ * O caractere que preenche possíveis buracos.
+ * @return A String no tamanho certo, preenchida ou cortada de acordo com o
+ * tamanho.
+ */
+fun leftFit(original: String, finalSize: Int, c: Char): String {
+	if (original.length == finalSize) {
+		return original
+	}
+	return if (original.length < finalSize) leftFill(original, finalSize, c) else leftCut(original, finalSize)
+}
 
-    /**
-     * Completa ou corta uma String de forma que ela fique com um tamanho fixo
-     * definido como parâmetro com alinhamento á ESQUERDA.
-     *
-     *
-     * @param original
-     * A Srtring original.
-     * @param finalSize
-     * O tamanho final que a String deve ter.
-     * @param c
-     * O caractere que preenche possíveis buracos.
-     * @return A String no tamanho certo, preenchida ou cortada de acordo com o
-     * tamanho.
-     */
-    fun leftFit(original: String, finalSize: Int, c: Char): String {
-        if (original.length == finalSize) {
-            return original
-        }
-        return if (original.length < finalSize) leftFill(original, finalSize, c) else leftCut(original, finalSize)
-    }
+/**
+ * Corta uma String de forma que ela fique com um tamanho fixo definido como
+ * parâmetro.
+ *
+ * @param align
+ * O tipo de alinhamento (direita ou esquerda).
+ *
+ * @param original
+ * A Srtring original.
+ * @param finalSize
+ * O tamanho final que a String deve ter.
+ * @return A String no tamanho certo, preenchida ou cortada de acordo com o
+ * tamanho.
+ */
+fun cut(align: Align, original: String, finalSize: Int): String {
+	return if (Align.RIGHT == align) leftCut(original, finalSize) else rightCut(original, finalSize)
+}
 
-    /**
-     * Corta uma String de forma que ela fique com um tamanho fixo definido como
-     * parâmetro.
-     *
-     * @param align
-     * O tipo de alinhamento (direita ou esquerda).
-     *
-     * @param original
-     * A Srtring original.
-     * @param finalSize
-     * O tamanho final que a String deve ter.
-     * @return A String no tamanho certo, preenchida ou cortada de acordo com o
-     * tamanho.
-     */
-    fun cut(align: Align, original: String, finalSize: Int): String {
-        return if (Align.RIGHT == align) leftCut(original, finalSize) else rightCut(original, finalSize)
-    }
+/**
+ * Corta uma String à direita até que ela fique no tamanho final definido
+ * como parâmetro.
+ *
+ * @param original
+ * A String original que será cortada.
+ * @param finalSize
+ * O tamanho final que a String deve ter.
+ * @return A String cortada e com o tamanho correto.
+ */
+fun leftCut(original: String, finalSize: Int): String {
+	return original.substring(original.length - finalSize, original.length)
+}
 
-    /**
-     * Corta uma String à direita até que ela fique no tamanho final definido
-     * como parâmetro.
-     *
-     * @param original
-     * A String original que será cortada.
-     * @param finalSize
-     * O tamanho final que a String deve ter.
-     * @return A String cortada e com o tamanho correto.
-     */
-    fun leftCut(original: String, finalSize: Int): String {
-        return original.substring(original.length - finalSize, original.length)
-    }
+/**
+ * Corta uma String à direita até que ela fique no tamanho final definido
+ * como parâmetro.
+ *
+ * @param original
+ * A String original que será cortada.
+ * @param finalSize
+ * O tamanho final que a String deve ter.
+ * @return A String cortada e com o tamanho correto.
+ */
+fun rightCut(original: String, finalSize: Int): String {
+	return original.substring(0, finalSize)
+}
 
-    /**
-     * Corta uma String à direita até que ela fique no tamanho final definido
-     * como parâmetro.
-     *
-     * @param original
-     * A String original que será cortada.
-     * @param finalSize
-     * O tamanho final que a String deve ter.
-     * @return A String cortada e com o tamanho correto.
-     */
-    fun rightCut(original: String, finalSize: Int): String {
-        return original.substring(0, finalSize)
-    }
+/**
+ * Completa uma String de forma que ela fique com um tamanho fixo definido
+ * como parâmetro.
+ *
+ * @param align
+ * O tipo de alinhamento (direita ou esquerda).
+ *
+ * @param original
+ * A Srtring original.
+ * @param finalSize
+ * O tamanho final que a String deve ter.
+ * @param c
+ * O caractere que preenche possíveis buracos.
+ * @return A String no tamanho certo, preenchida de acordo com o tamanho.
+ */
+fun fill(align: Align, original: String, fnalSize: Int, c: Char): String {
+	return if (Align.RIGHT == align) leftFill(original, fnalSize, c) else rightFill(original, fnalSize, c)
+}
 
-    /**
-     * Completa uma String de forma que ela fique com um tamanho fixo definido
-     * como parâmetro.
-     *
-     * @param align
-     * O tipo de alinhamento (direita ou esquerda).
-     *
-     * @param original
-     * A Srtring original.
-     * @param finalSize
-     * O tamanho final que a String deve ter.
-     * @param c
-     * O caractere que preenche possíveis buracos.
-     * @return A String no tamanho certo, preenchida de acordo com o tamanho.
-     */
-    fun fill(align: Align, original: String, fnalSize: Int, c: Char): String {
-        return if (Align.RIGHT == align) leftFill(original, fnalSize, c) else rightFill(original, fnalSize, c)
-    }
+/**
+ * Completa uma String á direita de forma que ela fique com um tamanho fixo
+ * definido como parâmetro.
+ *
+ * @param original
+ * A Srtring original.
+ * @param finalSize
+ * O tamanho final que a String deve ter.
+ * @param c
+ * O caractere que preenche possíveis buracos.
+ * @return A String no tamanho certo, preenchida de acordo com o tamanho.
+ */
+fun rightFill(original: String, finalSize: Int, c: Char): String {
+	val sb = StringBuilder(original)
+	for (i in original.length until finalSize) {
+		sb.append(c)
+	}
+	return sb.toString()
+}
 
-    /**
-     * Completa uma String á direita de forma que ela fique com um tamanho fixo
-     * definido como parâmetro.
-     *
-     * @param original
-     * A Srtring original.
-     * @param finalSize
-     * O tamanho final que a String deve ter.
-     * @param c
-     * O caractere que preenche possíveis buracos.
-     * @return A String no tamanho certo, preenchida de acordo com o tamanho.
-     */
-    fun rightFill(original: String, finalSize: Int, c: Char): String {
-        val sb = StringBuilder(original)
-        for (i in original.length until finalSize) {
-            sb.append(c)
-        }
-        return sb.toString()
-    }
-
-    /**
-     * Completa uma String à esquerda de forma que ela fique com um tamanho fixo
-     * definido como parâmetro.
-     *
-     * @param original
-     * A Srtring original.
-     * @param finalSize
-     * O tamanho final que a String deve ter.
-     * @param c
-     * O caractere que preenche possíveis buracos.
-     * @return A String no tamanho certo, preenchida de acordo com o tamanho.
-     */
-    fun leftFill(original: String, finalSize: Int, c: Char): String {
-        val sb = StringBuilder()
-        for (i in original.length until finalSize) {
-            sb.append(c)
-        }
-        sb.append(original)
-        return sb.toString()
-    }
-
+/**
+ * Completa uma String à esquerda de forma que ela fique com um tamanho fixo
+ * definido como parâmetro.
+ *
+ * @param original
+ * A Srtring original.
+ * @param finalSize
+ * O tamanho final que a String deve ter.
+ * @param c
+ * O caractere que preenche possíveis buracos.
+ * @return A String no tamanho certo, preenchida de acordo com o tamanho.
+ */
+fun leftFill(original: String, finalSize: Int, c: Char): String {
+	val sb = StringBuilder()
+	for (i in original.length until finalSize) {
+		sb.append(c)
+	}
+	sb.append(original)
+	return sb.toString()
 }
--- ppl-lib/src/test/java/br/net/buzu/ppllib/TextTest.java
+++ ppl-lib/src/test/java/br/net/buzu/ppllib/TextTest.java
@@ -1,10 +1,11 @@
 package br.net.buzu.ppllib;
 
 import static org.junit.Assert.*;
+import static br.net.buzu.ppllib.TextKt.*;
 
-import br.net.buzu.ppllib.*;
 
 import org.junit.Test;
+
 
 public class TextTest {
 	
@@ -12,39 +13,39 @@ public class TextTest {
 	@Test
 	public void testBlank(){
 		assertTrue(isBlank(null));
-		assertTrue(Text.INSTANCE.isBlank(""));
-		assertTrue(Text.INSTANCE.isBlank("    "));
-		assertTrue(Text.INSTANCE.isBlank("         "));
+		assertTrue(isBlank(""));
+		assertTrue(isBlank("    "));
+		assertTrue(isBlank("         "));
 		
-		assertFalse(Text.INSTANCE.isBlank("   0"));
-		assertFalse(Text.INSTANCE.isBlank("_______"));
-		assertFalse(Text.INSTANCE.isBlank("''''"));
+		assertFalse(isBlank("   0"));
+		assertFalse(isBlank("_______"));
+		assertFalse(isBlank("''''"));
 	}
 	
 	@Test
 	public void testLeftFit(){
-		assertEquals("12345", Text.INSTANCE.leftFit("12345", 5, '0'));
-		assertEquals("12345", Text.INSTANCE.leftFit("00012345", 5, '0'));
-		assertEquals("00123", Text.INSTANCE.leftFit("123", 5, '0'));
+		assertEquals("12345", leftFit("12345", 5, '0'));
+		assertEquals("12345", leftFit("00012345", 5, '0'));
+		assertEquals("00123", leftFit("123", 5, '0'));
 	}
 
 	@Test
 	public void testRightFit(){
-		assertEquals("12345", Text.INSTANCE.rightFit("12345", 5, '0'));
-		assertEquals("00012", Text.INSTANCE.rightFit("00012345", 5, '0'));
-		assertEquals("12300", Text.INSTANCE.rightFit("123", 5, '0'));
+		assertEquals("12345", rightFit("12345", 5, '0'));
+		assertEquals("00012", rightFit("00012345", 5, '0'));
+		assertEquals("12300", rightFit("123", 5, '0'));
 	}
 
 	@Test
 	public void testRightCut() {
-		assertEquals("a", Text.INSTANCE.rightCut("abcdef", 1));
-		assertEquals("ab", Text.INSTANCE.rightCut("abcdef", 2));
-		assertEquals("abc", Text.INSTANCE.rightCut("abcdef", 3));
-		assertEquals("abcd", Text.INSTANCE.rightCut("abcdef", 4));
-		assertEquals("abcde", Text.INSTANCE.rightCut("abcdef", 5));
-		assertEquals("abcdef", Text.INSTANCE.rightCut("abcdef", 6));
+		assertEquals("a", rightCut("abcdef", 1));
+		assertEquals("ab", rightCut("abcdef", 2));
+		assertEquals("abc", rightCut("abcdef", 3));
+		assertEquals("abcd", rightCut("abcdef", 4));
+		assertEquals("abcde", rightCut("abcdef", 5));
+		assertEquals("abcdef", rightCut("abcdef", 6));
 		try {
-			assertEquals("abcdef", Text.INSTANCE.rightCut("abcdef", 7));
+			assertEquals("abcdef", rightCut("abcdef", 7));
 			fail();
 		} catch (StringIndexOutOfBoundsException e) {
 
@@ -54,15 +55,15 @@ public class TextTest {
 
 	@Test
 	public void testleftCut() {
-		assertEquals("", Text.INSTANCE.leftCut("abcdef", 0));
-		assertEquals("f", Text.INSTANCE.leftCut("abcdef", 1));
-		assertEquals("ef", Text.INSTANCE.leftCut("abcdef", 2));
-		assertEquals("def", Text.INSTANCE.leftCut("abcdef", 3));
-		assertEquals("cdef", Text.INSTANCE.leftCut("abcdef", 4));
-		assertEquals("bcdef", Text.INSTANCE.leftCut("abcdef", 5));
-		assertEquals("abcdef", Text.INSTANCE.leftCut("abcdef", 6));
+		assertEquals("", leftCut("abcdef", 0));
+		assertEquals("f", leftCut("abcdef", 1));
+		assertEquals("ef", leftCut("abcdef", 2));
+		assertEquals("def", leftCut("abcdef", 3));
+		assertEquals("cdef", leftCut("abcdef", 4));
+		assertEquals("bcdef", leftCut("abcdef", 5));
+		assertEquals("abcdef", leftCut("abcdef", 6));
 		try {
-			assertEquals("abcdef", Text.INSTANCE.leftCut("abcdef", 7));
+			assertEquals("abcdef", leftCut("abcdef", 7));
 			fail();
 		} catch (StringIndexOutOfBoundsException e) {
 
@@ -71,14 +72,14 @@ public class TextTest {
 	
 	@Test
 	public void testRightFill(){
-		assertEquals("abcd00", Text.INSTANCE.rightFill("abcd", 6, '0'));
-		assertEquals("abc000", Text.INSTANCE.rightFill("abc", 6, '0'));
+		assertEquals("abcd00", rightFill("abcd", 6, '0'));
+		assertEquals("abc000", rightFill("abc", 6, '0'));
 	}
 
 	@Test
 	public void testLeftFill(){
-		assertEquals("00abcd", Text.INSTANCE.leftFill("abcd", 6, '0'));
-		assertEquals("000abc", Text.INSTANCE.leftFill("abc", 6, '0'));
+		assertEquals("00abcd", leftFill("abcd", 6, '0'));
+		assertEquals("000abc", leftFill("abc", 6, '0'));
 	}
 
 }
