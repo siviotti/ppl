18ba2f309fa01cb90a4656d28e110cf544538afa
07311631769 <07311631769@serpro-1537186>
2018-10-30 22:10:13 -0200

    ppl-lib

---
+++ ppl-lib/src/main/kotlin/br/net/buzu/ppllib/Ppl.kt
@@ -0,0 +1,8 @@
+package br.net.buzu.ppllib
+
+class Ppl {
+	
+	
+}
+
+fun add(a: Int, b: Int) = a + b
---
+++ ppl-lib/src/test/kotlin/br/net/buzu/ppllib/PplTest.kt
@@ -0,0 +1,11 @@
+package br.net.buzu.ppllib
+
+import org.junit.Test
+
+class PplTest {
+
+	@Test
+	fun testAdd() {
+		println( add(2,3) )
+	}
+}
